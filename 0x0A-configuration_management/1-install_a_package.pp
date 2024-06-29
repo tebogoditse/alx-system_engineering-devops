90281061afb1738d203ee8b49a1ba5c7387520f8
tebogoditse <tebogoditse2@gmail.com>
2024-06-29 22:05:50 +0200

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #Install package 'flask --version 2.1.0'
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+exec { 'install flask':
+  command =>  'pip3 install flask==2.1.0',
+  path    =>  '/usr/bin/',
+  unless  =>  'pip3 list | grep flask',
 }
