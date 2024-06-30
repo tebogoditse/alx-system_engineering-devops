f6bda6c3fe2382c6b911f43d8f601c8e10dbda1c
tebogoditse <tebogoditse2@gmail.com>
2024-06-30 03:48:48 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Install Package 'Flask --version 2.1.0' using puppet
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+
+
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
