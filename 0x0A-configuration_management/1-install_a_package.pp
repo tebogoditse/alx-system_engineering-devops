ceb6bd04fdebd1451cd359c7cd6a9c0c2c7c377f
tebogoditse <tebogoditse2@gmail.com>
2024-07-03 18:32:28 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
 # Install Package 'Flask --version 2.1.0' using puppet
 
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'pip3'
-}
-
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
-}
-
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['Flask']
 }
