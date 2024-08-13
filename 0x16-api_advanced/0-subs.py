c03c6d3dbffd2181fcb7e37132fa39f3c4fe7a4b
tebogoditse <tebogoditse2@gmail.com>
2024-08-13 23:56:46 +0200

    all

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Returns the number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 200:
+        data = res.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
